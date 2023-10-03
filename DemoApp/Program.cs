de7d912af89f458d9cc71f9c9211dc21ea3fbf88
mrtmdpro <minhducta4@gmail.com>
2023-10-03 19:54:14 +0300

    First commit - selection sort

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -2,9 +2,41 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+    class GFG
         {
-            Console.WriteLine("Hello, World!");
+            static void selectionSort(int[] arr)
+            {
+                int n = arr.Length;
+
+                for (int i = 0; i < n - 1; i++)
+                {
+                    int min_idx = i;
+                    for (int j = i + 1; j < n; j++)
+                        if (arr[j] < arr[min_idx])
+                            min_idx = j;
+
+                    int temp = arr[min_idx];
+                    arr[min_idx] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+
+            static void printArray(int[] arr)
+            {
+                int n = arr.Length;
+                for (int i = 0; i < n; ++i)
+                    Console.Write(arr[i] + " ");
+                Console.WriteLine();
+            }
+
+            public static void Main()
+            {
+                int[] arr = { 64, 25, 12, 22, 11 };
+                selectionSort(arr);
+                Console.WriteLine("Sorted array");
+                printArray(arr);
+            }
+
         }
     }
 }
