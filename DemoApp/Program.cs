bcdc42b1420c970a7d1bf4880b6169a8709d27f9
mrtmdpro <minhducta4@gmail.com>
2023-10-03 20:55:06 +0300

    Deleted selection sort and added additional.cs

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -6,23 +6,6 @@ namespace DemoApp
     {
     class GFG
         {
-            static void selectionSort(int[] arr)
-            {
-                int n = arr.Length;
-
-                for (int i = 0; i < n - 1; i++)
-                {
-                    int min_idx = i;
-                    for (int j = i + 1; j < n; j++)
-                        if (arr[j] < arr[min_idx])
-                            min_idx = j;
-
-                    int temp = arr[min_idx];
-                    arr[min_idx] = arr[i];
-                    arr[i] = temp;
-                }
-            }
-
             static void bubbleSort(int[] arr)
             {
                 int count = 0;
