2e8aa4773bbc64004f10daf3d38db94aee2be1fc
mrtmdpro <minhducta4@gmail.com>
2023-10-03 20:02:03 +0300

    Added bubble sort, changed main sorting algo to bubble sort

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace DemoApp
+﻿using System.Collections;
+
+namespace DemoApp
 {
     internal class Program
     {
@@ -21,6 +23,27 @@
                 }
             }
 
+            static void bubbleSort(int[] arr)
+            {
+                int count = 0;
+                bool flag = true;
+                for (int i = 1; (i <= (arr.Length - 1)) && flag; i++)
+                {
+                    flag = false;
+                    for (int j = 0; j < (arr.Length - 1); j++)
+                    {
+                        count = count + 1;
+                        if (arr[j + 1] > arr[j])
+                        {
+                            int temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                            flag = true;
+                        }
+                    }
+                }
+            }
+
             static void printArray(int[] arr)
             {
                 int n = arr.Length;
@@ -32,7 +55,7 @@
             public static void Main()
             {
                 int[] arr = { 64, 25, 12, 22, 11 };
-                selectionSort(arr);
+                bubbleSort(arr);
                 Console.WriteLine("Sorted array");
                 printArray(arr);
             }
